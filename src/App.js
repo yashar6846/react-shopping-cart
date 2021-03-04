1293b4f0de368dde85b1594118db9e39d9e8903b
yashar6846Qashqayi <69206232+gezal6846@users.noreply.github.com>
2021-03-04 03:23:29 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Cart } from './Components/Shopping/Cart/Cart';
+import { Min } from './Components/Shopping/Min/Min';
+import { Section } from './Components/Shopping/Section/Section';
+
+
+function App() {
+  return (
+    <div className="">
+      <Cart />
+      <Min />
+      <Section />
+    </div>
+  );
+}
+
+export default App;
